bcc5f77e7710dcf3b02f15cca95b26ca82d9b94c
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-11-25 22:11:52 +0200

    added 2nd solution to union.c

--- exam-rank-02/union.c
+++ exam-rank-02/union.c
@@ -1,3 +1,42 @@
+// solution 1
+
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int i;
+	int j;
+	unsigned char seen[256] = {0};
+
+	if (ac == 3)
+	{
+		i = 0;
+		while (av[1][i])
+		{
+			j = 0;
+			if (!seen[(unsigned char)av[1][i]])
+			{
+				write(1, &av[1][i], 1);
+				seen[(unsigned char)av[1][i]] = 1;
+			}
+			i++;
+		}
+		while (av[2][j])
+		{
+			if (!seen[(unsigned char)av[2][j]])
+			{
+				write(1, &av[2][j], 1);
+				seen[(unsigned char)av[2][j]] = 1;
+			}
+			j++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+// solution 2
+
 #include <unistd.h>
 
 int	main(int ac, char **av)
