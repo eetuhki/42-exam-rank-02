45bf9d8ef2a2b9b0bc1a067c1682765c35d42668
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-11-29 10:33:16 +0200

    Update ft_rrange.c

--- exam-rank-02/ft_rrange.c
+++ exam-rank-02/ft_rrange.c
@@ -9,22 +9,19 @@ int	ft_abs(int n)
 
 int	*ft_rrange(int start, int end)
 {
-	int	*array;
-	int	i;
+	int	*res;
 	int	len;
-	int	step;
 
-	i = -1;
-	len = 1 + ft_abs(end - start);
-	array = malloc(sizeof(int) * len);
-	if (start > end)
-		step = 1;
-	else
-		step = -1;
-	while (++i < len)
+	len = ft_abs(end - start) + 1;
+	res = malloc(sizeof(int) * len);
+	if (!res)
+		return (NULL);
+	while (len--)
 	{
-		array[i] = end;
-		end = end + step;
+		if (start < end)
+			res[len] = start++;
+		else
+			res[len] = start--;
 	}
-	return (array);
+	return (res);
 }
