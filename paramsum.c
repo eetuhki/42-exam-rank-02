65cfa6c591fcc365a4982fb122c66ab71b05a741
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-12-02 12:18:36 +0200

    Add files via upload

---
+++ paramsum.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void	putnbr(int n)
+{
+	char	c;
+
+	if (n > 9)
+		putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	(void)av;
+	int	i;
+
+	i = 0;
+	while (i < ac)
+		i++;
+	putnbr(i - 1);
+	write(1, "\n", 1);
+	return (0);
+}
