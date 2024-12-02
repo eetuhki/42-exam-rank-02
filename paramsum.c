0c2c74ef3d257ec5063efc9dba5eb1f5f90e98ac
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-12-02 12:20:44 +0200

    streamlined paramsum

--- paramsum.c
+++ paramsum.c
@@ -13,12 +13,8 @@ void	putnbr(int n)
 int	main(int ac, char **av)
 {
 	(void)av;
-	int	i;
 
-	i = 0;
-	while (i < ac)
-		i++;
-	putnbr(i - 1);
+	putnbr(ac - 1);
 	write(1, "\n", 1);
 	return (0);
 }
