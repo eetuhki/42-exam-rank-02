cd92426b28f2e26deafa0bb7bb5739d100d7010c
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-12-02 12:03:46 +0200

    streamlined inter.c

--- inter.c
+++ inter.c
@@ -2,30 +2,24 @@
 
 int	main(int ac, char **av)
 {
-	int i;
-	int	j;
-	unsigned char first[256] = {0};
+	int				i;
+	int				j;
+	unsigned char	seen[256] = {0};
 
 	if (ac == 3)
 	{
-		i = 0;
-		while (av[1][i])
+		i = -1;
+		while (av[1][++i])
 		{
-			j = 0;
-			while (av[2][j])
+			j = -1;
+			while (av[2][++j])
 			{
-				if (av[1][i] == av[2][j])
+				if (av[1][i] == av[2][j] && (!seen[(unsigned char)av[1][i]]))
 				{
-					if (!first[(unsigned char)av[1][i]])
-					{
-						write(1, &av[1][i], 1);
-						first[(unsigned char)av[1][i]] = 1;
-					}
-					break;
+					write(1, &av[1][i], 1);
+					seen[(unsigned char)av[1][i]] = 1;
 				}
-				j++;
 			}
-			i++;
 		}
 	}
 	write(1, "\n", 1);
