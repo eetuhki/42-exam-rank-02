c148b062278e425c0879aba13ad786720926abcf
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-12-03 11:38:45 +0200

    streamlined alpha_mirror

--- alpha_mirror.c
+++ alpha_mirror.c
@@ -4,11 +4,9 @@ int	main(int ac, char **av)
 {
 	int	i;
 
-	i = -1;
-	if (ac != 2)
-		write(1, "\n", 1);
-	else
+	if (ac == 2)
 	{
+		i = -1;
 		while (av[1][++i])
 		{
 			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
@@ -17,7 +15,7 @@ int	main(int ac, char **av)
 				av[1][i] = 219 - av[1][i];
 			write(1, &av[1][i], 1);
 		}
-		write(1, "\n", 1);
 	}
+	write(1, "\n", 1);
 	return (0);
 }
