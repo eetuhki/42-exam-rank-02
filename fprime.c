ce8809d6e63e868b548be479baad77242fe254bd
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-12-10 11:04:51 +0200

    restructure

--- fprime.c
+++ fprime.c
@@ -3,28 +3,28 @@
 
 int	main(int ac, char **av)
 {
-	int i;
-	int n;
+	int	i;
+	int	n;
 
-	i = 1;
-	if (ac != 2)
-		return(printf("\n"));
-	if (av[1])
-		n = atoi(av[1]);
-	if (n == 1)
-		printf("1");
-	while (n >= ++i)
+	if (ac == 2)
 	{
-		if (n % i == 0)
+		n = atoi(av[1]);
+		if (n == 1)
+			printf("1");
+		i = 1;
+		while (i++ < n)
 		{
-			printf("%u", i);
-			if (n == i)
-				break ;
-			printf("*");
-			n /= i;
-			i = 1;
+			if (!(n % i))
+			{
+				printf("%d", i);
+				if (n == i)
+					break;
+				printf("*");
+				n /= i;
+				i = 1;
+			}
 		}
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
