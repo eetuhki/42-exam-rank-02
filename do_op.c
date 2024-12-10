918c252cca4c9f542962c60f18c81dd1a1ee5305
Eetu Laine <167189172+eetuhki@users.noreply.github.com>
2024-12-10 11:19:40 +0200

    removed duplicate atoi

--- do_op.c
+++ do_op.c
@@ -1,32 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
-	int	value1;
-	int	value2;
-	
+	int	n1;
+	int	n2;
 
-	if (av[1] && av[3])
+	if (ac == 4)
 	{
-		value1 = atoi(av[1]);
-		value2 = atoi(av[3]);
-	}
-	if (value1 && value2 && ac == 4)
-	{
-		value1 = atoi(av[1]);
-		value2 = atoi(av[3]);
+		n1 = atoi(av[1]);
+		n2 = atoi(av[3]);
 		if (av[2][0] == '+')
-			printf("%d", (value1 + value2));
+			printf("%d", (n1 + n2));
 		if (av[2][0] == '-')
-			printf("%d", (value1 - value2));
+			printf("%d", (n1 - n2));
 		if (av[2][0] == '*')
-			printf("%d", (value1 * value2));
+			printf("%d", (n1 * n2));
 		if (av[2][0] == '/')
-			printf("%d", (value1 / value2));
+			printf("%d", (n1 / n2));
 		if (av[2][0] == '%')
-			printf("%d", (value1 % value2));
+			printf("%d", (n1 % n2));
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
